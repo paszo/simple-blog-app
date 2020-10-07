9441032b7caeda2a9ace7032826cba5ef5d43e94
paszo <paszo2008@gmail.com>
2020-10-07 20:46:58 +0200

    added mongoose and db

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const dbURI = process.env.DBURI || 'database';
-console.log(dbURI);
+const dbURI = process.env.DBURI;
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    });
+});
+
 
 app.get('/', (req, res) => {
     res.send('hello main master');
 })
 
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
